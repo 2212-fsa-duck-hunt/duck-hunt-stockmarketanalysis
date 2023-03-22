be9757a84c7a1aada7f6fb74ccc1bf1294abebef
Michelle <mzou1223@gmail.com>
2023-03-22 18:10:11 -0400

    GOT MY NEWS WORKING ON EVERY TAB BABY LOAD IT UP

---
+++ app/news/energy/page.jsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useEffect, useState } from 'react';
+import "../../../public/news.css"
+
+const listOfStocks = require('../../listOfStocks.JSON');
+
+
+export default function Energy() {
+    const [data, setData] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const energyList = listOfStocks.filter((element) => { return element.Sector === 'Energy' });
+    const ilSymbols = energyList.map(element => element.Symbol);
+
+    useEffect(() => {
+        setIsLoading(true);
+        const promises = ilSymbols.map(symbol =>
+            fetch(`https://api.polygon.io/v2/reference/news?ticker=${symbol}&limit=1&apiKey=p3DDXEob7V6iRw5653VW9k_bEkGXG6hj`, {
+                method: "GET",
+                headers: {
+                    "X-Polygon-Edge-ID": "sample_edge_id",
+                    "X-Polygon-Edge-IP-Address": "8.8.8.8",
+                },
+            })
+                .then(res => res.json())
+        );
+        Promise.all(promises)
+            .then(data => setData(data));
+        setIsLoading(false);
+    }, []);
+
+    if (!data) {
+        return <div> im loading .__.</div>
+    } else {
+        return (
+            <div>
+                <section className="sec">
+                    <h3 id> Energy </h3>
+                    {data.map(stock =>
+                        <div className='newsbox'>
+                            <h4> {stock.results[0].title}</h4>
+                            <img className="picture" src={stock.results[0].image_url} />
+                            <div className="description"> {stock.results[0].description}</div>
+                        </div>
+                    )}
+                </section>
+            </div>
+        )
+    }
+}
