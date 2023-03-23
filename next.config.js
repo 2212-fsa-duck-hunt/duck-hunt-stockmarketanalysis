8b34da1215c34a4e335cc537240b190e1aed2879
sshih1116 <sshih1116@gmail.com>
2023-03-23 11:47:19 -0700

    deleted useless code from bugfixing

--- next.config.js
+++ next.config.js
@@ -3,23 +3,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  async headers() {
-    return [
-      {
-        source: "/watchlist",
-        headers: [
-          {
-            key: "X-Polygon-Edge-ID",
-            value: "sample_edge_id",
-          },
-          {
-            key: "X-Polygon-Edge-IP-Address",
-            value: "8.8.8.8",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig
