47603770492bcf3e5634a4b6a604d493d6f640b6
Alex Bernt <alexbernt100@gmail.com>
2023-03-27 12:56:28 -0500

    Small changes to be more efficient

--- app/TopGainers.jsx
+++ app/TopGainers.jsx
@@ -182,15 +182,15 @@ export default function TopGainers() {
   ];
 
   if (allStocks.length < 500) {
-    return <h1>Loading</h1>;
+    return <h1>Fetching the latest market data</h1>;
   }
 
   const sortedStock = allStocks
+    .filter((elem) => elem.session.change_percent > 0)
     .sort(
       (firstItem, secondItem) =>
         secondItem.session.change_percent - firstItem.session.change_percent
-    )
-    .filter((elem) => elem.session.change_percent > 0);
+    );
 
   return (
     <Box>
