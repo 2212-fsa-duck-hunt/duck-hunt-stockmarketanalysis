cd33687c7fcebd781d47f9c1ae8e17f9b3991f0b
Alex Bernt <alexbernt100@gmail.com>
2023-03-27 20:56:08 -0500

    Add styling to login pages

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -5,6 +5,7 @@ import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import { Typography } from "@mui/material";
 import Router, { useRouter } from "next/navigation";
 
 export default function SignUp() {
@@ -34,7 +35,6 @@ export default function SignUp() {
     <div id="home-container">
       <div id="home-icon-container4">
         <div>
-          <h1>Registration:</h1>
           <Box
             component="form"
             sx={{
@@ -43,26 +43,35 @@ export default function SignUp() {
             noValidate
             autoComplete="off"
             onSubmit={signUp}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            flexDirection="column"
           >
-            <div>
-              <TextField
-                required
-                id="standard-required"
-                label="Email"
-                defaultValue={email}
-                onChange={(e) => setEmail(e.target.value)}
-                variant="standard"
-              />
-              <TextField
-                id="standard-password-input"
-                label="Password"
-                type="password"
-                defaultValue={password}
-                onChange={(e) => setPassword(e.target.value)}
-                variant="standard"
-              />
-            </div>
-            <Button type="submit" variant="text">
+            <Typography variant="h3" marginBottom={1} marginTop={10}>
+              Signup
+            </Typography>
+            <TextField
+              required
+              id="standard-required"
+              label="Email"
+              defaultValue={email}
+              onChange={(e) => setEmail(e.target.value)}
+              variant="outlined"
+            />
+            <TextField
+              id="standard-password-input"
+              label="Password"
+              type="password"
+              defaultValue={password}
+              onChange={(e) => setPassword(e.target.value)}
+              variant="outlined"
+            />
+            <Button
+              type="submit"
+              variant="outlined"
+              sx={{ color: "black", borderColor: "black" }}
+            >
               Signup
             </Button>
             <>{signUpMsg}</>
