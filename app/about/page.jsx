9ead969f26e7495846fd90890e2534d88478098e
Alex Bernt <alexbernt100@gmail.com>
2023-04-03 16:44:52 -0500

    about

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,6 +1,47 @@
-export default function About() {
+"use client";
+import { Typography, Box, Grid, Paper } from "@mui/material";
+import "../stocks/styles.css";
+import Image from "next/image";
 
-    return (<div>
-        <h1> about page for now. it goes to the route /about </h1>
-    </div>)
+export default function About() {
+  return (
+    <div>
+      <Typography
+        variant={"h3"}
+        display="flex"
+        justifyContent="center"
+        style={{
+          fontFamily: "Poppins",
+          marginTop: 25,
+        }}
+        sx={{ color: "#53588c" }}
+      >
+        About Us
+      </Typography>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container columnSpacing={3} marginTop={10}>
+          <Grid item xs={3}>
+            <Paper
+              style={{
+                justifyContent: "center",
+                display: "flex",
+                backgroundColor: "#e0e2ef",
+              }}
+            >
+              <img src="Alex.png" height="auto" width="250" />
+            </Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper>Hello</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper>Hello</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper>Hello</Paper>
+          </Grid>
+        </Grid>
+      </Box>
+    </div>
+  );
 }
